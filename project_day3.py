cf5f30e5eff8394ea19d3f894568536127d71fad
Syed Saad (王泽） <87109729+Saad-data@users.noreply.github.com>
2022-07-08 01:50:37 +0800

    mac

---
+++ project_day3.py
@@ -0,0 +1,57 @@
+"""
+ \       /
+  \     /
+   \.-./
+  (o\^/o)  _   _   _     __
+   ./ \.\ ( )-( )-( ) .-'  '-.
+    {-} \(//  ||   \\/ (   )) '-.
+         //-__||__.-\\.       .-'
+        (/    ()     \)'-._.-'
+        ||    ||      \\
+        ('    ('       ')
+
+              "=.
+             "=. \
+                \ \
+             _,-=\/=._        _.-,_
+            /         \      /=-._ "-.
+           |=-./~\___/~\    /     `-._\
+           |   \o/   \o/   /         /
+            \_   `~~~;/    |         |
+              `~,._,-'    /          /
+                 | |      =-._      /
+             _,-=/ \=-._     /|`-._/
+           //           \\   )\
+          /|             |)_.'/
+         //|             |\_."   _.-\
+        (|  \           /    _.`=    \
+        ||   ":_    _.;"_.-;"   _.-=.:
+     _-."/    / `-."\_."        =-_.;\
+    `-_./   /             _.-=.    / \\
+           |              =-_.;\ ."   \\
+           \                   \\/     \\
+           /\_                .'\\      \\
+          //  `=_         _.-"   \\      \\
+         //      `~-.=`"`'       ||      ||
+         ||    _.-_/|            ||      |\_.-_
+     _.-_/|   /_.-._/            |\_.-_  \_.-._\
+    /_.-._/                      \_.-._\
+"""
+
+print("Welcome to ant game")
+a=input("Is this a man or female any? m for Male and f for female")
+if a == "f":
+    b=input("How do you know i'm female? do i look hot? y or n")
+    if b == "y":
+        print("Your so sweet but know what?\n I'M married ")
+    else:
+        ("bye then")
+else:
+    print("GAME OVER because you don't have eyes can't you see beautiful ant")
+
+
+
+
+
+
+
